53a25d439fa4581c01e6f72fa52f533b34988fbd
Jeff Widmer <jeff@townbackyard.com>
2020-08-19 11:54:35 -0400

    Add the createComment sproc

---
+++ BlogWebApp/CosmosDbScripts/sprocs/createComment.js
@@ -0,0 +1,24 @@
+﻿function createComment(postId, comment) {
+    var collection = getContext().getCollection();
+
+    collection.readDocument(
+        `${collection.getAltLink()}/docs/${postId}`,
+        function (err, post) {
+            if (err) throw err;
+
+            post.commentCount++;
+            collection.replaceDocument(
+                post._self,
+                post,
+                function (err) {
+                    if (err) throw err;
+
+                    comment.postId = postId;
+                    collection.createDocument(
+                        collection.getSelfLink(),
+                        comment
+                    );
+                }
+            );
+        })
+}
