63ad758c8bcc7cadb04c5b1cb8136972447e09a7
Jeff Widmer <jeff@townbackyard.com>
2020-09-09 12:50:25 -0400

    Check if the like exists for the userId and the postId and delete it and then decrement the post.likeCount.  Make sure that the user has already liked this post before changing the likeCount.

--- BlogWebApp/CosmosDbScripts/sprocs/deleteLike.js
+++ BlogWebApp/CosmosDbScripts/sprocs/deleteLike.js
@@ -1,36 +1,39 @@
 ﻿function deleteLike(postId, userId) {
     var collection = getContext().getCollection();
 
-    collection.readDocument(
-        `${collection.getAltLink()}/docs/${postId}`,
-        function (err, post) {
-            if (err) throw err;
+    var likeQuery = {
+        query: "SELECT * FROM p WHERE p.postId=@postId AND p.type ='like' AND p.userId = @userId",
+        parameters: [{ name: "@postId", value: postId }, { name: "@userId", value: userId }]
+    };
 
-            if (post.likeCount > 0) {
-                post.likeCount--;
-            }
-            else {
-                post.likeCount = 0;
-            }
-            collection.replaceDocument(
-                post._self,
-                post,
-                function (err) {
-                    if (err) throw err;
+    collection.queryDocuments(collection.getSelfLink(), likeQuery, function (err, feed, responseOptions) {
+        if (err) throw err;
 
-                    var likeQuery = {
-                        query: "SELECT * FROM p WHERE p.postId=@postId AND p.type ='like' AND p.userId = @userId",
-                        parameters: [{ name: "@postId", value: postId }, { name: "@userId", value: userId }]
-                    };
-                    collection.queryDocuments(collection.getSelfLink(), likeQuery, function (err, feed, responseOptions) {
+        if (feed.length >= 1) {
+            collection.deleteDocument(feed[0]._self, {}, function (err, responseOptions) {
+                if (err) throw err;
+
+                collection.readDocument(
+                    `${collection.getAltLink()}/docs/${postId}`,
+                    function (err, post) {
                         if (err) throw err;
-                        if (feed.length >= 1) {
-                            collection.deleteDocument(feed[0]._self, {}, function (err, responseOptions) {
-                                if (err) throw err;
-                            });
+
+                        if (post.likeCount > 0) {
+                            post.likeCount--;
                         }
-                    });
-                }
-            );
-        })
+                        else {
+                            post.likeCount = 0;
+                        }
+                        collection.replaceDocument(
+                            post._self,
+                            post,
+                            function (err) {
+                                if (err) throw err;
+                            }
+                        );
+                    })
+            });
+        }
+    });
+
 }
